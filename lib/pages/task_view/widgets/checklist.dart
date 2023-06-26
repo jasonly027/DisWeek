aca048e546ba0a94580c1bf20ebe5369355d5f30
jasonly027 <81354745+jasonly027@users.noreply.github.com>
2023-06-25 18:39:56 -0700

    Reorderable Checklist

--- lib/pages/task_view/widgets/checklist.dart
+++ lib/pages/task_view/widgets/checklist.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../../../utils/task.dart';
 
@@ -28,106 +29,168 @@ class _checklistState extends State<checklist> {
         children: [
           widget.checks.isEmpty
               ? const SizedBox.shrink()
-              : ListView.builder(
+              : ReorderableListView(
                   shrinkWrap: true,
                   physics: const NeverScrollableScrollPhysics(),
-                  itemCount: widget.checks.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Row(
-                      key: UniqueKey(),
-                      children: [
-                        Expanded(
-                          flex: 2,
-                          child: Checkbox(
-                              value: widget.checks[index].isChecked,
-                              activeColor: theme.primary,
-                              onChanged: (bool? value) {
-                                setState(() {
-                                  widget.checks[index].isChecked = value!;
-                                });
-                              }),
-                        ),
-                        Expanded(
-                          flex: 9,
-                          child: TextFormField(
-                            initialValue: widget.checks[index].title,
-                            textInputAction: TextInputAction.done,
-                            onChanged: (text) {
-                              widget.checks[index].title = text;
-                            },
-                            onTapOutside: (context) {
-                              FocusManager.instance.primaryFocus?.unfocus();
-                            },
-                            maxLines: null,
-                            style: const TextStyle(fontSize: 17),
-                            decoration: const InputDecoration(
-                                contentPadding: EdgeInsets.all(5),
-                                border: InputBorder.none),
-                          ),
-                        ),
-                        Expanded(
-                          flex: 2,
-                          child: IconButton(
-                            onPressed: (){
-                              setState(() {
-                                widget.checks.removeAt(index);
-                              });
-                            },
-                            icon: const Icon(Icons.close),
-                          )
-                        )
-                      ],
-                    );
+                  // itemCount: widget.checks.length,
+                  buildDefaultDragHandles: true,
+                  onReorder: (oldIndex, tempNewIndex) {
+                    setState(() {
+                      final newIndex = tempNewIndex > oldIndex
+                          ? tempNewIndex - 1
+                          : tempNewIndex;
+                      final item = widget.checks.removeAt(oldIndex);
+                      widget.checks.insert(newIndex, item);
+                    });
                   },
+                  onReorderStart: (index) => HapticFeedback.lightImpact(),
+                  children: widget.checks
+                      .map((item) => Row(
+                            key: ValueKey(item),
+                            children: [
+                              Expanded(
+                                flex: 2,
+                                child: Checkbox(
+                                    value: item.isChecked,
+                                    activeColor: theme.primary,
+                                    onChanged: (bool? value) {
+                                      setState(() {
+                                        item.isChecked = value!;
+                                      });
+                                    }),
+                              ),
+                              Expanded(
+                                flex: 9,
+                                child: TextFormField(
+                                  initialValue: item.title,
+                                  textInputAction: TextInputAction.done,
+                                  onChanged: (text) {
+                                    item.title = text;
+                                  },
+                                  onTapOutside: (context) {
+                                    FocusManager.instance.primaryFocus
+                                        ?.unfocus();
+                                  },
+                                  maxLines: null,
+                                  style: const TextStyle(fontSize: 17),
+                                  decoration: const InputDecoration(
+                                      contentPadding: EdgeInsets.all(5),
+                                      border: InputBorder.none),
+                                ),
+                              ),
+                              Expanded(
+                                  flex: 2,
+                                  child: IconButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        widget.checks.remove(item);
+                                      });
+                                    },
+                                    icon: const Icon(Icons.close),
+                                  )),
+                              Expanded(
+                                  flex: 2,
+                                  child: IconButton(
+                                      onPressed: () {},
+                                      icon: const Icon(Icons.drag_indicator)))
+                            ],
+                          ))
+                      .toList(),
+                  // itemBuilder: (BuildContext context, int index) {
+                  //   return Row(
+                  //     key: UniqueKey(),
+                  //     children: [
+                  //       Expanded(
+                  //         flex: 2,
+                  //         child: Checkbox(
+                  //             value: widget.checks[index].isChecked,
+                  //             activeColor: theme.primary,
+                  //             onChanged: (bool? value) {
+                  //               setState(() {
+                  //                 widget.checks[index].isChecked = value!;
+                  //               });
+                  //             }),
+                  //       ),
+                  //       Expanded(
+                  //         flex: 9,
+                  //         child: TextFormField(
+                  //           initialValue: widget.checks[index].title,
+                  //           textInputAction: TextInputAction.done,
+                  //           onChanged: (text) {
+                  //             widget.checks[index].title = text;
+                  //           },
+                  //           onTapOutside: (context) {
+                  //             FocusManager.instance.primaryFocus?.unfocus();
+                  //           },
+                  //           maxLines: null,
+                  //           style: const TextStyle(fontSize: 17),
+                  //           decoration: const InputDecoration(
+                  //               contentPadding: EdgeInsets.all(5),
+                  //               border: InputBorder.none),
+                  //         ),
+                  //       ),
+                  //       Expanded(
+                  //           flex: 2,
+                  //           child: IconButton(
+                  //             onPressed: () {
+                  //               setState(() {
+                  //                 widget.checks.removeAt(index);
+                  //               });
+                  //             },
+                  //             icon: const Icon(Icons.close),
+                  //           ))
+                  //     ],
+                  //   );
+                  // },
                 ),
-              // : ListView(
-              //     shrinkWrap: true,
-              //     children: widget.checks
-              //         .map((item) => Row(
-              //               children: [
-              //                 Expanded(
-              //                   flex: 2,
-              //                   child: Checkbox(
-              //                       value: item.isChecked,
-              //                       activeColor: theme.primary,
-              //                       onChanged: (bool? value) {
-              //                         setState(() {
-              //                           item.isChecked = value!;
-              //                         });
-              //                       }),
-              //                 ),
-              //                 Expanded(
-              //                   flex: 9,
-              //                   child: TextFormField(
-              //                     initialValue: item.title,
-              //                     textInputAction: TextInputAction.done,
-              //                     onChanged: (text) {
-              //                       item.title = text;
-              //                     },
-              //                     onTapOutside: (context) {
-              //                       FocusManager.instance.primaryFocus
-              //                           ?.unfocus();
-              //                     },
-              //                     maxLines: null,
-              //                     style: const TextStyle(fontSize: 17),
-              //                     decoration: const InputDecoration(
-              //                         contentPadding: EdgeInsets.all(5),
-              //                         border: InputBorder.none),
-              //                   ),
-              //                 ),
-              //                 Expanded(
-              //                     flex: 2,
-              //                     child: IconButton(
-              //                       onPressed: () {
-              //                         setState(() {
-              //                           widget.checks.remove(item);
-              //                         });
-              //                       },
-              //                       icon: const Icon(Icons.close),
-              //                     ))
-              //               ],
-              //             ))
-              //         .toList()),
+          // : ListView(
+          //     shrinkWrap: true,
+          //     children: widget.checks
+          //         .map((item) => Row(
+          //               children: [
+          //                 Expanded(
+          //                   flex: 2,
+          //                   child: Checkbox(
+          //                       value: item.isChecked,
+          //                       activeColor: theme.primary,
+          //                       onChanged: (bool? value) {
+          //                         setState(() {
+          //                           item.isChecked = value!;
+          //                         });
+          //                       }),
+          //                 ),
+          //                 Expanded(
+          //                   flex: 9,
+          //                   child: TextFormField(
+          //                     initialValue: item.title,
+          //                     textInputAction: TextInputAction.done,
+          //                     onChanged: (text) {
+          //                       item.title = text;
+          //                     },
+          //                     onTapOutside: (context) {
+          //                       FocusManager.instance.primaryFocus
+          //                           ?.unfocus();
+          //                     },
+          //                     maxLines: null,
+          //                     style: const TextStyle(fontSize: 17),
+          //                     decoration: const InputDecoration(
+          //                         contentPadding: EdgeInsets.all(5),
+          //                         border: InputBorder.none),
+          //                   ),
+          //                 ),
+          //                 Expanded(
+          //                     flex: 2,
+          //                     child: IconButton(
+          //                       onPressed: () {
+          //                         setState(() {
+          //                           widget.checks.remove(item);
+          //                         });
+          //                       },
+          //                       icon: const Icon(Icons.close),
+          //                     ))
+          //               ],
+          //             ))
+          //         .toList()),
           SizedBox(
             width: double.infinity,
             child: TextButton(
